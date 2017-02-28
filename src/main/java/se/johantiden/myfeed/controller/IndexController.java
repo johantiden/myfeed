ffa975ed6643642a966db0e4c17fc6d6e94244c3
Johan Tidén <johan.tiden@looklet.com>
2017-02-28 22:45:53 +0100

    Stub list instead of single item

--- src/main/java/se/johantiden/myfeed/controller/IndexController.java
+++ src/main/java/se/johantiden/myfeed/controller/IndexController.java
@@ -1,17 +1,23 @@
 package se.johantiden.myfeed.controller;
 
+import com.google.common.collect.Lists;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.johantiden.myfeed.output.OutputBean;
+
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
 public class IndexController {
 
     @RequestMapping("/rest/index")
-    OutputBean ping() {
-        return new OutputBean("hej", "http://www.google.com", "http://www.google.com/thumb.png");
+    List<OutputBean> index() {
+        return Lists.newArrayList(
+                new OutputBean("hej1", "http://www.google.com", "http://www.google.com/thumb.png"),
+                new OutputBean("hej2", "http://www.google.com", "http://www.google.com/thumb.png")
+        );
     }
 
 }
