d5620e78089873c715cee2204d0bd1d73fb2ae60
Johan Tidén <johan.tiden@looklet.com>
2017-02-28 21:13:17 +0100

    spring boot

---
+++ src/main/java/se/johantiden/myfeed/MyfeedApplication.java
@@ -0,0 +1,12 @@
+package se.johantiden.myfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfeedApplication.class, args);
+	}
+}
