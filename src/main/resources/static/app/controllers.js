e9b28407a1c6890a7af5f033b22c353c11d797df
Johan Tidén <johan.tiden@looklet.com>
2017-04-13 23:59:02 +0200

    filter

--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -115,6 +115,9 @@ app.controller('myCtrl', function($scope, $location, $sce, $cookies, $window, do
                 (isFrom(i, "reddit") && categoryContains(i, "bidenbro")) ||
                 (isFrom(i, "reddit") && categoryContains(i, "woahdude")) ||
                 (isFrom(i, "reddit") && categoryContains(i, "photoshopbattles")) ||
+                (isFrom(i, "reddit") && categoryContains(i, "overwatch")) ||
+                (isFrom(i, "reddit") && categoryContains(i, "animalcrossing")) ||
+                (isFrom(i, "reddit") && categoryContains(i, "empiredidnothingwrong")) ||
 
                 contains(i, 'join us') ||
                 contains(i, 'upvote') ||
