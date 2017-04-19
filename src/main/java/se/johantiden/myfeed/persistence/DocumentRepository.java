2b7d94ec42aad30c53b2e3f5810fd70b315008ae
Johan Tidén <johan.tiden@looklet.com>
2017-04-19 21:47:44 +0200

    Oops purge old always deletes everything :D

--- src/main/java/se/johantiden/myfeed/persistence/DocumentRepository.java
+++ src/main/java/se/johantiden/myfeed/persistence/DocumentRepository.java
@@ -1,10 +1,10 @@
 package se.johantiden.myfeed.persistence;
 
 import se.johantiden.myfeed.persistence.redis.Key;
+import se.johantiden.myfeed.util.Chrono;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,11 +14,7 @@ import java.util.stream.Collectors;
 
 public class DocumentRepository {
 
-    public static final Comparator<Document> YOUNGEST_FIRST =
-            Comparator.comparing(Document::getPublishDate);
-
     Map<Key<Document>, Document> map = new HashMap<>();
-
 
     public void put(Document document) {
         map.put(document.getKey(), document);
@@ -41,6 +37,9 @@ public class DocumentRepository {
     }
 
     private Predicate<? super Map.Entry<Key<Document>, Document>> isOlderThan(Duration duration) {
-        return e -> e.getValue().getPublishDate().plus(duration).isAfter(Instant.now());
+        return e -> {
+            Instant publishDate = e.getValue().getPublishDate();
+            return Chrono.isOlderThan(duration, publishDate);
+        };
     }
 }
