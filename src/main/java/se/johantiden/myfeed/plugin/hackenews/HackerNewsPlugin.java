56fb7bad77beb8861eb66008261fa9c16dba19bc
Johan Tidén <johan.tiden@looklet.com>
2017-04-26 22:28:28 +0200

    Suppress parse exception

--- src/main/java/se/johantiden/myfeed/plugin/hackenews/HackerNewsPlugin.java
+++ src/main/java/se/johantiden/myfeed/plugin/hackenews/HackerNewsPlugin.java
@@ -13,6 +13,7 @@ import se.johantiden.myfeed.plugin.Plugin;
 import se.johantiden.myfeed.plugin.rss.RssPlugin;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
@@ -47,29 +48,41 @@ public class HackerNewsPlugin implements Plugin {
     }
 
     private static Double findVotes(Document document) {
-        org.jsoup.nodes.Document rssDocument = Jsoup.parse(document.html);
-        String commentsUrl = rssDocument.select("a").get(0).attr("href");
+        try {
+            org.jsoup.nodes.Document rssDocument = Jsoup.parse(document.html);
+            String commentsUrl = rssDocument.select("a").get(0).attr("href");
 
-        org.jsoup.nodes.Document jsoupDocument = getJsoupDocument(commentsUrl);
+            org.jsoup.nodes.Document jsoupDocument = getJsoupDocument(commentsUrl);
 
-        Elements select = jsoupDocument.select(".score");
-        if (select.size() == 1) {
-            Element element = select.get(0);
-            String html = element.html();
-            html = html.replace(" points", "");
+            Elements select = jsoupDocument.select(".score");
+            if (select.size() == 1) {
+                Element element = select.get(0);
+                String html = element.html();
+                html = html.replace(" points", "");
 
-            int i = Integer.parseInt(html);
-            return (double) i;
+                int i = Integer.parseInt(html);
+                return (double) i;
+            }
+            return null;
+        } catch (RuntimeException e) {
+            log.error("Couldn't parse HackerNews votes:", e);
+            return 9999.0;
         }
-        return null;
     }
 
     private static org.jsoup.nodes.Document getJsoupDocument(String pageUrl) {
+        return getDocumentInner(pageUrl, 3);
+    }
+
+    private static org.jsoup.nodes.Document getDocumentInner(String pageUrl, int retriesLeft) {
         try {
             return Jsoup.parse(new URL(pageUrl), 10_000);
         } catch (IOException e) {
-            log.error("Could not jsoup-parse " + pageUrl, e);
-            return null;
+            if (retriesLeft > 0) {
+                return getDocumentInner(pageUrl, retriesLeft - 1);
+            } else {
+                throw new UncheckedIOException("Could not jsoup-parse " + pageUrl, e);
+            }
         }
     }
 
