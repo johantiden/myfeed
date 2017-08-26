a7a3fdb3d360649f754a1e529d6b97a914483b63
Johan Tidén <johan.tiden@looklet.com>
2017-08-26 13:58:55 +0200

    Suppress feed read exception logging.

--- src/main/java/se/johantiden/myfeed/reader/FeedReaderService.java
+++ src/main/java/se/johantiden/myfeed/reader/FeedReaderService.java
@@ -1,20 +1,24 @@
 package se.johantiden.myfeed.reader;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.johantiden.myfeed.persistence.Document;
 import se.johantiden.myfeed.persistence.Feed;
 import se.johantiden.myfeed.plugin.FeedReader;
 import se.johantiden.myfeed.util.DocumentPredicates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class FeedReaderService {
+
+    private static final Logger log = LoggerFactory.getLogger(FeedReaderService.class);
 
     private FeedReaderService() {
     }
 
     public static List<Document> readAll(Feed feed) {
-        FeedReader reader = feed.getFeedReader();
-        List<Document> documents = reader.readAllAvailable();
+        List<Document> documents = getDocuments(feed);
 
         documents.stream()
                 .filter(DocumentPredicates.hasEscapeCharacters())
@@ -24,6 +28,17 @@ public final class FeedReaderService {
                 });
 
         return documents;
+    }
+
+    private static List<Document> getDocuments(Feed feed) {
+        FeedReader reader = feed.getFeedReader();
+        try {
+            return reader.readAllAvailable();
+        } catch (RuntimeException e) {
+            log.error("Failed to read feed {}", feed.getName());
+            log.debug("Exception:", e);
+            return new ArrayList<>();
+        }
     }
 
 }
