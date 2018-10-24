5a366680e2adb5ff7ad5095f134fca63cf4ee1dc
Johan Tidén <johan.tiden@looklet.com>
2018-10-24 19:34:38 +0200

    Elm is the new main

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd src/main/elm
 ./compile.sh
 cd ../../..
 mkdir -p target/classes/static
-mv src/main/elm/o.html target/classes/static/index2.html
+mv src/main/elm/o.html target/classes/static/index.html
 
 
 mvn spring-boot:run
