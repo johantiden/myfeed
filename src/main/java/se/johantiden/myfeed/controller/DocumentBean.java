73c93a3232c2484ee5c1f8fd8f28930d027550d8
Johan Tidén <johan.tiden@looklet.com>
2018-10-28 22:16:17 +0100

    Clean

--- src/main/java/se/johantiden/myfeed/controller/DocumentBean.java
+++ src/main/java/se/johantiden/myfeed/controller/DocumentBean.java
@@ -12,7 +12,6 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class DocumentBean {
